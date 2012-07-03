e24b5f7a790b4d9747828b627c485f2c46c20dc5
Paul Duran <paulduran@gmail.com>
2012-07-04 00:23:36 +1000

    initial work to add jira issues content provider

---
+++ Blaze/ContentProviders/JiraIssuesContentProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using JabbR.ContentProviders.Core;
+
+namespace JabbR.ContentProviders
+{
+    public class JiraIssuesContentProvider : CollapsibleContentProvider
+    {
+        private static readonly Regex JiraIssuesRegex = new Regex(@"(https?://.*?)/browse/([A-Z]+-\d+)");
+        private const string JiraIssuesApiFormat = "{0}/rest/api/latest/issue/{1}?jsonp-callback=addJiraIssue";
+        private const string JiraIssuesContentFormat = "<div class='jira-issue jira-issue-{0}'></div><script src='{1}'></script>";
+
+        protected override ContentProviderResultModel GetCollapsibleContent(Uri uri)
+        {
+            var parameters = ExtractParameters(uri);
+
+            return new ContentProviderResultModel
+                {
+                Content = String.Format(JiraIssuesContentFormat,
+                        parameters[1],
+                    String.Format(JiraIssuesApiFormat, parameters[0], parameters[1])
+                ),
+                Title = uri.AbsoluteUri
+            };
+        }
+
+        protected override Regex ParameterExtractionRegex
+        {
+            get
+            {
+                return JiraIssuesRegex;
+            }
+        }
+
+        protected override bool IsValidContent(Uri uri)
+        {
+            return ExtractParameters(uri).Count == 2;
+        }
+    }
+}
---
+++ Blaze/Scripts/chat/Chat.jiraissues.js
@@ -0,0 +1,28 @@
+﻿(function ($, window, ui) {
+    'use strict';
+
+    window.addJiraIssue = function (issue) {
+        // Keep track of whether we're need the end, so we can auto-scroll once the tweet is added.
+        var nearEnd = ui.isNearTheEnd(),
+            elements = null;
+
+        elements = $('div.jira-issue-' + issue.key)
+            .removeClass('jira-issue-' + issue.key);
+
+
+        issue.fields.description = chat.utility.markdownToHtml(issue.fields.description);
+
+        // Process the template, and add it in to the div.
+        $('#jira-issues-template').tmpl(issue.fields).appendTo(elements);
+
+        // After the string has been added to the template etc, remove any existing targets and re-add with _blank
+        $('a', elements).removeAttr('target').attr('target', '_blank');
+
+        $('.js-relative-date').timeago();
+        // If near the end, scroll.
+        if (nearEnd) {
+            ui.scrollToBottom();
+        }        
+    };
+
+})(jQuery, window, chat.ui);
